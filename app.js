709dddc4af642fb671dcb33fbdeca2b7ba54e50e
huseyin <huseyinarslan06@gmail.com>
2022-06-04 16:22:46 +0300

    capturing

--- app.js
+++ app.js
@@ -13,13 +13,22 @@ let productDiv = document.querySelector(".products");
 
 productDiv.addEventListener("click", (event) => {
     if (event.target.className == "minus") {
+        if (event.target.nextElementSibling.innerText > 1) {
+            event.target.nextElementSibling.innerText--;
+        } else {
+            if (confirm("Product will be deleted?")) {
+                event.target.parentElement.parentElement.parentElement.remove();
+            }
+        }
         // console.log("minus button clicked");
+        
     } else if (event.target.classList.contains("plus")) {
         // console.log("plus button clicked");
+        event.target.previousElementSibling.innerText++;
     } else if (event.target.classList.contains("remove-product")) {
-        console.log("remove button clicked");
+        // console.log("remove button clicked");
+        event.target.parentElement.parentElement.parentElement.remove();
     } else {
         // console.log("other element is clicked");
     }
-
 })
